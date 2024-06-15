f2cf50c5996e9fd0c22d46bc8b0458ed9333a35b
HanWeiOng <hanwei.ong.2022@scis.smu.edu.sg>
2024-06-15 12:07:28 +0800

    Added modules

--- Agentic-RAG/VectorDB/index.py
+++ Agentic-RAG/VectorDB/index.py
@@ -11,15 +11,25 @@ gpt4all_kwargs = {'allow_download': 'True'}
 def createVectorDB(urls: list, chunkSize: int, chunkOverlap: int, apiKey: str):
     # Scrape through specified URLs    
     #TODO
+    docs_list = [FireCrawlLoader(api_key=apiKey, url=url, mode="scrape").load() for url in urls]
 
     # Split documentss 
     #TODO
+    TEXTSPLITTER= RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=chunkSize,
+        chunk_overlap=chunkOverlap
+    )
 
     # Split documents by chunks
     #TODO
+    docs_split_by_chunks=TEXTSPLITTER.split_documents(docs_list)
 
     # Filter out complex metadata and ensure proper document formatting
     #TODO
+    docs_filtered=[]
+    for doc in docs_split_by_chunks:
+        if isinstance(doc, Document) and hasattr(doc,'metadata'):
+            clean_metadata={key:value for key, value in doc.metadata.items() if isinstance(value,str,int,float,bool)}
     
     # Add to vectorDB and save as retriever as a variable
     #TODO
